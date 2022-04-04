aa5c7acad50ca8b146d67044a29496883480e2a2
KrSergei <34679066+KrSergei@users.noreply.github.com>
2022-04-05 00:27:40 +0300

    Configured control animation

--- FinalTask/Assets/Scripts/Player/PlayerAnimationConrtol.cs
+++ FinalTask/Assets/Scripts/Player/PlayerAnimationConrtol.cs
@@ -6,7 +6,7 @@ public class PlayerAnimationConrtol : MonoBehaviour
 {
     Animator _anim;
     [SerializeField] private string _currentState;
-
+    [SerializeField] private string _currentKeyName;
     void Start()
     {
         _anim = GetComponent<Animator>();
@@ -19,8 +19,22 @@ public class PlayerAnimationConrtol : MonoBehaviour
     public void ChangeState(string newState)
     {
         if (newState == _currentState) return;
-        if (_anim) _anim.Play("Base Layer." + newState);
+        if (_anim) _anim.Play("Base Layer." + newState, 0, 0.5f);
         _currentState = newState;
+    }
 
+    public void ChangeAnimationByParametr(string newParametr)
+    {
+        if (_currentKeyName.Equals(newParametr)) return;
+        _currentKeyName = newParametr;
+        _anim.SetTrigger(newParametr);
+        Debug.Log("_currentParametr = " + _currentKeyName);
+    }
+    public void ChangeAnimationByBoolParametr(string newParametr, bool state)
+    {
+        if (_currentKeyName == newParametr) return;
+        _currentKeyName = newParametr;
+        _anim.SetBool(newParametr, state);
+        Debug.Log("_currentKeyName = " + _currentKeyName);
     }
 }
--- FinalTask/Assets/Scripts/Player/PlayerControl.cs
+++ FinalTask/Assets/Scripts/Player/PlayerControl.cs
@@ -9,55 +9,105 @@ public class PlayerControl : MonoBehaviour
     private const string PLAYER_WALK_FORWARD_STATE = "Player_WalkForward";
     private const string PLAYER_RUN_FORWARD_STATE = "Player_RunForward";
     //private const string PLAYER_WALK_BACKWARD_STATE = "WalkBackward";
+
+    private const string PLAYER_IDLE_PARAMETR = "IDLE";
+    private const string PLAYER_WALK_FORWARD_PARAMETR = "WalkForwardTr";
+    private const string PLAYER_RUN_FORWARD_PARAMETR = "RunForwardTr";
+    private const string PLAYER_WALK_BACKWARD_PARAMETR = "WalkBackwardTr";
+
     #endregion
 
     [SerializeField] private bool _isRunning = false;
 
     public PlayerAnimationConrtol playerAnimationConrtol;
 
-    //[SerializeField] private float _walkSpeed = 1f;
-    //[SerializeField] private float _runSpeed = 2f;
+    [SerializeField] private float _walkSpeed = 1f;
+    [SerializeField] private float _runSpeed = 2f;
+    [SerializeField] private Vector3 _directionToMove;
 
     CharacterController _cc;
-
-
     // Start is called before the first frame update
     void Start()
     {
         //Get charater controller
         _cc = GetComponent<CharacterController>();
         playerAnimationConrtol = GetComponent<PlayerAnimationConrtol>();
+        _directionToMove = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckingIsRunningState();
-        //if (Input.GetKeyDown(KeyCode.LeftShift)) _isRunning = true;
-        //else _isRunning = false;
+        //CheckingIsRunningState();
 
-        //Если состояния игрока "бег"
-        if(_isRunning)
-        {
-
-            //если нажата клавиша W, проигрывание анимации Player_RunForward
-            if (Input.GetKey(KeyCode.W)) playerAnimationConrtol.ChangeState(PLAYER_RUN_FORWARD_STATE);
-        }
-        else
-        {
-            //если нажата клавиша W, проигрывание анимации Player_WalkForward
-            if (Input.GetKey(KeyCode.W)) playerAnimationConrtol.ChangeState(PLAYER_WALK_FORWARD_STATE);
-
-        }
-
-        //if (Input.GetKey(KeyCode.LeftShift))
+        //если нажата клавиша W, проигрывание анимации Player_WalkForward
+        //if (Input.GetKey(KeyCode.W))
         //{
-        //    //если нажата только одна клавиша "режим бега", проигрывание анимации IDLE
-        //    playerAnimationConrtol.ChangeState(PLAYER_IDLE_STATE);
+        //    _directionToMove = Vector3.forward * _walkSpeed;
+        //    playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_WALK_FORWARD_PARAMETR);
+        //    ++ _countPressedbutton;
+        //    Debug.Log("_countPressedbutton = " + _countPressedbutton);
+        //}
+        ////если нажата клавиша S, проигрывание анимации Player_WalkBackword
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_WALK_BACKWARD_PARAMETR);
+        //    _directionToMove = -Vector3.forward * _walkSpeed;
         //}
 
-        //если не нажата ни одна клавиша, проигрывание анимации IDLE
-        if (Input.anyKey == false) playerAnimationConrtol.ChangeState(PLAYER_IDLE_STATE);
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    playerAnimationConrtol.ChangeAnimationByBoolParametr(PLAYER_WALK_BACKWARD_PARAMETR, true);
+        //    _directionToMove = -Vector3.forward * _walkSpeed;
+        //}
+
+        ////if (_isRunning == false)
+        ////{
+
+        ////}
+        ////if (_isRunning)
+        ////{
+        ////    //если нажата клавиша W, проигрывание анимации Player_RunForward
+        ////    if (Input.GetKey(KeyCode.W))
+        ////    {
+        ////        playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_RUN_FORWARD_PARAMETR);
+        ////        _directionToMove = Vector3.forward * _runSpeed;
+        ////    } 
+        ////}
+
+        ////если скорость персонажа равна 0, проигрывание анимации IDLE
+        //if (_cc.velocity == Vector3.zero) playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_IDLE_PARAMETR);
+        ////расчет вектора движения
+        //_directionToMove *= Time.deltaTime;
+        //StartCoroutine(Move(_directionToMove));
+
+        
+        //если нажата клавиша W, проигрывание анимации Player_WalkForward
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_WALK_FORWARD_PARAMETR);
+            _directionToMove = Vector3.forward * _walkSpeed;
+        }
+        //если нажата клавиша S, проигрывание анимации Player_WalkBackword
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_WALK_BACKWARD_PARAMETR);
+            _directionToMove = -Vector3.forward * _walkSpeed;
+        }
+
+        
+        //если скорость персонажа равна 0, проигрывание анимации IDLE
+        if (_cc.velocity == Vector3.zero) playerAnimationConrtol.ChangeAnimationByParametr(PLAYER_IDLE_PARAMETR);
+        //_directionToMove *= Time.deltaTime;
+        //StartCoroutine(Move(_directionToMove));
+
+    }
+
+    private void FixedUpdate()
+    {
+        //расчет вектора движения
+        _directionToMove *= Time.deltaTime;
+        StartCoroutine(Move(_directionToMove));
     }
 
     /// <summary>
@@ -67,5 +117,11 @@ public class PlayerControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftShift)) _isRunning = true;
         else _isRunning = false;
+    }
+
+    IEnumerator Move(Vector3 newDirection)
+    {
+        _cc.Move(newDirection);
+        yield return null;
     }
 }
